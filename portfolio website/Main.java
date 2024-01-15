6132629be34cc7f084dea4eac44a0333346ef492
Anitha-Ezhil <115613482+Anitha-Ezhil@users.noreply.github.com>
2024-01-15 16:52:03 +0530

    Add files via upload

---
+++ portfolio website/Main.java
@@ -0,0 +1,29 @@
+import java.io.*;
+public class Main{
+    void sort(int arr[]){
+        int n=arr.length; 
+        for(int i=0;i<n;i++){
+            int min_index=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[min_index]){
+                    arr[min_index]=arr[i];
+                    arr[i]=temp;
+                }
+            }
+        }
+    }
+    void printarray (int arr[]){
+        int n=arr.length;
+        for(int i=0;i<n;++i){
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+    public static void main(String[args]){
+        selectionsort ob=
+    }
+    ]
+
+
+        }
+    }
+}
